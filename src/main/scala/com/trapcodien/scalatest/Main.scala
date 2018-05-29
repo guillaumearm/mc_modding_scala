4f729b96c06e3ace6bc84dfa0f03e196dfebb43d
Guillaume ARM <trapcodien@gmail.com>
2018-05-29 17:34:25 +0200

    feat: prepare proxies and events

--- src/main/scala/com/trapcodien/scalatest/Main.scala
+++ src/main/scala/com/trapcodien/scalatest/Main.scala
@@ -1,31 +1,77 @@
 package com.trapcodien.scalatest
 
 
-import net.minecraft.init.Blocks
-import net.minecraftforge.fml.common.Mod
+import com.trapcodien.scalatest.MyMod.logger
+import net.minecraft.block.Block
+import net.minecraft.block.material.Material
+import net.minecraft.item.Item
+import net.minecraftforge.fml.common.{Mod, SidedProxy}
 import net.minecraftforge.fml.common.Mod.EventHandler
-import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent}
+import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.event.RegistryEvent
 import org.apache.logging.log4j.Logger
+
+class FirstBlock extends Block(Material.ROCK) {
+    logger foreach (_.info("***************** => instanciate first block"))
+}
+
+class CommonProxy {
+
+  def preInit(event: FMLPreInitializationEvent): Unit = {
+    logger foreach (_.info("*********** => PRE INIT"))
+  }
+
+  def init(event: FMLInitializationEvent): Unit = {
+    logger foreach(_.info("********* => INIT"))
+  }
+
+  def postInit(event: FMLPostInitializationEvent): Unit = {
+    logger foreach(_.info("******** => POST INIT"))
+  }
+
+
+  def registerBlocks(event: RegistryEvent.Register[Block]): Unit = {
+    logger foreach (_.info("===================> REGISTER FIRST BLOCK"))
+  }
+
+  def registerItems(event: RegistryEvent.Register[Item]): Unit = {
+    logger foreach (_.info("===================> REGISTER ITEM"))
+  }
+}
 
 
 @Mod(modid = MyMod.MODID, version = MyMod.VERSION, modLanguage = "scala")
+@Mod.EventBusSubscriber(modid = MyMod.MODID)
 object MyMod {
-  type PreInitEvent = FMLPreInitializationEvent
-  type InitEvent = FMLInitializationEvent
-
   final val MODID = "scalatest"
   final val VERSION = "0.1.0"
 
-  private var logger: Option[Logger] = None
+  var logger: Option[Logger] = None
+
+  @Mod.Instance
+  val instance: MyMod.type = this
+
+  @SidedProxy(clientSide = "com.trapcodien.scalatest.CommonProxy", serverSide = "com.trapcodien.scalatest.CommonProxy")
+  var proxy: CommonProxy = new CommonProxy
+
+
 
   @EventHandler
-  def preInit(event: PreInitEvent): Unit = {
+  def preInit(event: FMLPreInitializationEvent): Unit = {
     logger = Some(event.getModLog)
-    logger foreach(_.info("INIT"))
+    proxy.preInit(event)
   }
 
   @EventHandler
-  def init(event: InitEvent): Unit = {
-    logger foreach(_.info(s"DIRT BLOCK >> ${Blocks.DIRT.toString()}"))
-  }
+  def init(event: FMLInitializationEvent): Unit = proxy.init(event)
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent): Unit = proxy.postInit(event)
+
+  @SubscribeEvent
+  def registerBlocks(event: RegistryEvent.Register[Block]): Unit = proxy.registerBlocks(event)
+
+  @SubscribeEvent
+  def registerItems(event: RegistryEvent.Register[Item]): Unit = proxy.registerItems(event)
 }
